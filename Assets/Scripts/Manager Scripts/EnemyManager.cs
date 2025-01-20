f2a8effb9464e8875e77d6ac7a515de9c058b9be
Fibeer1 <fibeer67@gmail.com>
2025-01-20 12:54:59 +0100

    started work on the enemy spawner

--- Assets/Scripts/Manager Scripts/EnemyManager.cs
+++ Assets/Scripts/Manager Scripts/EnemyManager.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private Transform enemyUnitParent;
+    [SerializeField] private Transform spawnCircle;
+    private Player player;
+    public static List<Enemy> enemies = new List<Enemy>();
+    public static List<Entity> activeAllies = new List<Entity>();
+    public float spawnDistance;
+    [SerializeField] private float enemySpawnTime;
+    private float enemySpawnTimer;
+    [SerializeField] private bool shouldSpawnEnemies = false;
+
+    private void Start()
     {
-        
+        enemySpawnTimer = enemySpawnTime;
+        player = FindObjectOfType<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        HandleSpawnTimer();
+    }
+
+    private void HandleSpawnTimer()
+    {
+        if (!shouldSpawnEnemies)
+        {
+            return;
+        }
+        enemySpawnTimer -= Time.deltaTime;
+        if (enemySpawnTimer <= 0)
+        {
+            HandleEnemySpawning();
+        }
+    }
+
+    private void HandleEnemySpawning()
+    {
+        enemySpawnTimer = enemySpawnTime;
+    }
+
+    private void SpawnEnemy(int enemyIndex)
+    {
+        //Spawn the enemy in a radius around the player
+        float angle = Random.Range(-360, 360);
+        Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+        Vector3 position = player.transform.position + (spawnDistance * direction);
+
+        GameObject enemyInstance = Instantiate(enemyPrefabs[enemyIndex], position, Quaternion.identity, enemyUnitParent);
+        Enemy enemyScript = enemyInstance.GetComponent<Enemy>();
+        enemies.Add(enemyScript);
     }
 }
