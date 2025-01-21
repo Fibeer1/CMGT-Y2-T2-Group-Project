56f13aee057cbba436e7b5d1b0f3559537515592
Sjoerd <554839@student.saxion.nl>
2025-01-21 10:53:49 +0100

    Added ability CD overlay

--- Assets/Scripts/UI Scripts/HUD.cs
+++ Assets/Scripts/UI Scripts/HUD.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
     private Player player;
+    [Header("HUD Elements")]
+    [SerializeField] private Image MeleeCDOverlay;
+    [SerializeField] private Image DashCDOverlay;
+    [SerializeField] private Image RangedCDOverlay;
+    [SerializeField] private Image ShieldCDOverlay;
     [SerializeField] private Slider healthSlider;
-
+    
     private void Start()
     {
         player = FindObjectOfType<Player>();
@@ -17,6 +22,10 @@ public class HUD : MonoBehaviour
 
     private void Update()
     {
+        MeleeCDOverlay.fillAmount = player.swordSwingCDTimer / player.swordSwingCD;
+        DashCDOverlay.fillAmount = player.dashCDTimer / player.dashCD;
+        RangedCDOverlay.fillAmount = player.rangedAttackCDTimer / player.rangedAttackCD;
+        ShieldCDOverlay.fillAmount= player.shieldCDTimer / player.shieldCD;
         healthSlider.value = player.health;
         healthSlider.maxValue = player.maxHealth;
     }
